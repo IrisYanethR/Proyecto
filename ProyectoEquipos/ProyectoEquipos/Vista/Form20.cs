03f5612af6b7238d2f4656f23b93939be2fc64d9
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2020-10-11 12:22:52 -0600

    Wilian

---
+++ ProyectoEquipos/ProyectoEquipos/Vista/Form20.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoEquipos.Vista
+{
+    public partial class Form20 : Form
+    {
+        public Form20()
+        {
+            InitializeComponent();
+        }
+    }
+}
